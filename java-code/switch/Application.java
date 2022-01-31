1e8bb476bfb12e16d70aa1bcd466cb177a93ad01
Alma Rahat <a.a.m.rahat@swansea.ac.uk>
2022-01-31 11:39:45 +0000

    errors fixed in switch

--- java-code/switch/Application.java
+++ java-code/switch/Application.java
@@ -4,16 +4,10 @@ public class Application
   // it throws InterruptedException to properly shut down Threads.
   public static void main(String[] args) throws InterruptedException
   {
+    // create a switch instance
+    Switch sw1 = new Switch("sw1");
     // create new thread for switch
-    Thread toggler = new Thread(new Switch("sw1"));
-    // start executing the thread
-    toggler.start();
-    // the main thread sleeps while switch keeps going
-    Thread.sleep(2000); // for 2 secs
-    // stopping the other thread
-    toggler.interrupt();
-    // allow toggler to finish what it is doing
-    toggler.join();
+    Thread toggler = new Thread(sw1);
     // start executing the thread
     toggler.start();
     // the main thread sleeps while switch keeps going
