155180d315b60c5c4deecd9208a1625fd539fa83
Alma Rahat <a.a.m.rahat@swansea.ac.uk>
2022-02-11 13:43:34 +0000

    removed further unnecessary comments.

--- java-code/ornamentalgarden/Application.java
+++ java-code/ornamentalgarden/Application.java
@@ -1,5 +1,3 @@
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import java.util.Scanner;
 
 public class Application {
--- java-code/ornamentalgarden/Turnstile.java
+++ java-code/ornamentalgarden/Turnstile.java
@@ -1,6 +1,3 @@
-import java.util.logging.Level;
-import java.util.logging.Logger;
-
 public class Turnstile implements Runnable{
     private Counter people;
 
